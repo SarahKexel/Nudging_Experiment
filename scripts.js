c626704cf80e698ed1fb46a2dd0badf90eb2061b
Jaakko Nurkka <jaakkonurkka@gmail.com>
2018-02-24 09:01:07 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+new Vue({
+  el: '#app',
+  data: {
+    heading: 'Nudging experiment',
+    message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas dui id ornare arcu odio ut sem nulla. Commodo elit at imperdiet dui accumsan. Vitae semper quis lectus nulla at volutpat. Arcu non sodales neque sodales ut etiam. Massa id neque aliquam vestibulum morbi blandit cursus. Aliquet risus feugiat in ante metus dictum at tempor commodo. Consequat id porta nibh venenatis cras sed. Pretium lectus quam id leo in vitae turpis. Elementum integer enim neque volutpat ac tincidunt vitae. Aenean sed adipiscing diam donec adipiscing tristique risus nec feugiat. Turpis tincidunt id aliquet risus.'
+  }
+})
