691b97592f5cff9ce5188ed4eccff8dc7b62f083
Jaakko Nurkka <jaakkonurkka@gmail.com>
2018-02-24 14:04:39 +0100

    Add basic functionality

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-console.log(Math.floor(Math.random() * Math.floor(3)));
+
+document.getElementById("demo").innerHTML = "Hello " + getRandomNumber(4);
+
+function getRandomNumber(n) {
+  return Math.floor(Math.random() * Math.floor(n));
+}
+
+function navigateToTestPage() {
+  localStorage.setItem('test', 'sarah');
+  window.location='test.html';
+}
