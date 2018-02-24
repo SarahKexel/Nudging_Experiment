ae0dc8f5ca95be789b71b141e552dab04541728b
Sarah <sarahkexel@Sarahs-MBP.fritz.box>
2018-02-24 16:44:46 +0100

    add new pages

--- scripts.js
+++ scripts.js
@@ -5,7 +5,36 @@ function getRandomNumber(n) {
   return Math.floor(Math.random() * Math.floor(n));
 }
 
-function navigateToTestPage() {
+function navigateToContextPage() {
   localStorage.setItem('test', 'sarah');
-  window.location='test.html';
+  window.location='context.html';
+}
+
+function navigateToTestquestionsPage() {
+  localStorage.setItem('test', 'sarah');
+  window.location='testquestions.html';
+}
+
+function navigateToProductdescriptionPage() {
+  localStorage.setItem('test', 'sarah');
+  window.location='productdescription.html';
+}
+
+function navigateToProductchoiceTomatosPage() {
+  localStorage.setItem('test', 'sarah');
+  window.location='productchoiceTomatos.html';
+}
+function navigateToTendencyPage() {
+  localStorage.setItem('test', 'sarah');
+  window.location='tendency.html';
+}
+
+function navigateToShoppingcartPage() {
+  localStorage.setItem('test', 'sarah');
+  window.location='shoppingcart.html';
+}
+
+function navigateToFinalPage() {
+  localStorage.setItem('test', 'sarah');
+  window.location='final.html';
 }
