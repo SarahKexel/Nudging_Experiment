1b5026f9d6a59a3e1279497ec0fb738cd91916fa
Sarah <sarahkexel@Sarahs-MacBook-Pro.local>
2018-03-15 20:05:53 +0100

    Bio Bilder geändert, -disclosure Text angepasst, Testwuestion nach 2 mal weiterleiten

--- handleTestquestions.js
+++ handleTestquestions.js
@@ -1,10 +1,19 @@
+var answercounter = 0;
+
 function testSelection() {
+  console.log(answercounter);
   if(document.getElementById('optionsfeldRadio1').checked && document.getElementById('optionsfeldRadio5').checked){
     document.getElementById('no-selection').classList.add('d-none');
     window.location='productdescription.html';
   }
   else {
     //show warning if selection false
-    document.getElementById('no-selection').classList.remove('d-none');
+    if (answercounter > 1){
+      window.location='productdescription.html';
+    }
+    else {
+      document.getElementById('no-selection').classList.remove('d-none');
+      answercounter = answercounter + 1;
+    }
   }
 }
